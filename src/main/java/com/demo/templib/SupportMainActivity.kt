05e3992feb7227d9920f6ad10311e88fe8815072
sra <Shamantula_Ravikanth@shure.com>
2022-04-25 16:13:22 +0530

    Initial commit

---
+++ src/main/java/com/demo/templib/SupportMainActivity.kt
@@ -0,0 +1,11 @@
+package com.demo.templib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SupportMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_support_main)
+    }
+}
