1dfd29e925c868801eb2108ba20cfacc5f66365d
sra <Shamantula_Ravikanth@shure.com>
2022-04-25 16:34:17 +0530

    Changed visibility of the Activity

--- src/main/java/com/demo/templib/SupportMainActivity.kt
+++ src/main/java/com/demo/templib/SupportMainActivity.kt
@@ -3,7 +3,7 @@ package com.demo.templib
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SupportMainActivity : AppCompatActivity() {
+public class SupportMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support_main)
